13f29b5bee2691644a90d07e743107944dc46bb2
akhiang <akhiang32@gmail.com>
2018-05-18 00:44:05 +0700

    test

--- beranda.php
+++ beranda.php
@@ -2,32 +2,25 @@
 <html>
 <head>
 	<title></title>
-
-	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <!-- <link rel="stylesheet" type="text/css" href="css/datepicker.css"> -->
+  <script src="javascript/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+  <!-- <script src="javascript/bootstrap-datepicker.js"></script> -->
 </head>
 <body>
 		<div id="beranda">
 			Welcome To SkyCom
 		</div>
-		<div class="form-group has-error has-feedback">
-      <label class="col-sm-2 control-label" for="inputError">Input with error and glyphicon</label>
-      <div class="col-sm-5">
-        <input type="text" class="form-control" id="inputError">
-        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-      </div>
-    </div>
 
-            <div class="form-group col-lg-4">
-            <div class="form-group has-feedback">
-                <label class="control-label" for="inputValidation">Validation </label>
-                <input type="text" class="form-control" id="inputValidation" placeholder="Search"/>
-                <span class="glyphicon glyphicon-search form-control-feedback"></span>
-            </div>
-        </div>
+<div class="input-group mb-3 input-group-sm">
+  <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <span class="input-group-text"><ion-icon name="search"></ion-icon></span>
+  </div>
+</div>
 
+<script src="https://unpkg.com/ionicons@4.1.1/dist/ionicons.js"></script>
 </body>
 </html>
