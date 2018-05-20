eaae9dd8a02d004d817c23f2131ff71a4c8ae7b7
Xiaowilz <37570392+Xiaowilz@users.noreply.github.com>
2018-05-20 13:54:25 +0700

    tambah edit customer

--- sql/db_skycom.sql
+++ sql/db_skycom.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2018 at 08:37 PM
+-- Generation Time: May 20, 2018 at 08:53 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -120,7 +120,9 @@ CREATE TABLE `tb_hd_pembelian` (
 --
 
 INSERT INTO `tb_hd_pembelian` (`notrans`, `kd_barang`, `nm_barang`, `qty`, `harga`, `jumlah`) VALUES
-('TB00001', 'VGA_00002', 'Gigabyte GTX 1070T TI', 1, 9550000, 9550000);
+('TB00001', 'VGA_00001', 'Asus GTX 1080 TI', 1, 15000000, 15000000),
+('TB00001', 'VGA_00002', 'Gigabyte GTX 1070T TI', 1, 9550000, 9550000),
+('TB00002', 'VGA_00001', 'Asus GTX 1080 TI', 1, 15000000, 15000000);
 
 -- --------------------------------------------------------
 
@@ -142,26 +144,7 @@ CREATE TABLE `tb_hd_penjualan` (
 --
 
 INSERT INTO `tb_hd_penjualan` (`no_trans`, `kd_barang`, `nm_barang`, `qty`, `harga`, `jumlah`) VALUES
-('TJ-00001', 'VGA_00002', 'Gigabyte GTX 1070T TI', 10, 9550000, 95500000),
-('TJ-00001', 'VGA_00001', 'Asus GTX 1080 TI', 10, 15000000, 150000000),
-('TJ-00001', 'VGA_00002', 'Gigabyte GTX 1070T TI', 20, 9550000, 191000000),
-('TJ-00002', 'VGA_00001', 'Asus GTX 1080 TI', 1, 15000000, 15000000),
-('TJ-00002', 'VGA_00002', 'Gigabyte GTX 1070T TI', 12, 9550000, 114600000),
-('TJ-00002', 'VGA_00001', 'Asus GTX 1080 TI', 100, 15000000, 1500000000),
-('', 'VGA_00001', 'Asus GTX 1080 TI', 1, 15000000, 15000000),
-('TJ-00003', 'VGA_00002', 'Gigabyte GTX 1070T TI', 10, 9550000, 95500000),
-('TJ-00004', 'VGA_00002', 'Gigabyte GTX 1070T TI', 20, 9550000, 191000000),
-('TJ-00005', 'VGA_00001', 'Asus GTX 1080 TI', 10, 15000000, 150000000),
-('TJ-00006', 'VGA_00002', 'Gigabyte GTX 1070T TI', 25, 9550000, 238750000),
-('TJ-00007', 'VGA_00001', 'Asus GTX 1080 TI', 200, 15000000, 3000000000),
-('TJ-00008', 'VGA_00001', 'Asus GTX 1080 TI', 2222, 15000000, 33330000000),
-('TJ-00009', 'VGA_00001', 'Asus GTX 1080 TI', 21, 15000000, 315000000),
-('', 'VGA_00001', 'Asus GTX 1080 TI', 1, 15000000, 15000000),
-('', 'VGA_00002', 'Gigabyte GTX 1070T TI', 1, 9550000, 9550000),
-('', 'VGA_00002', 'Gigabyte GTX 1070T TI', 1, 9550000, 9550000),
-('TJ-00010', 'VGA_00002', 'Gigabyte GTX 1070T TI', 20, 9550000, 191000000),
-('TJ-00010', 'VGA_00001', 'Asus GTX 1080 TI', 20, 15000000, 300000000),
-('TJ-00011', 'VGA_00001', 'Asus GTX 1080 TI', 2, 15000000, 30000000);
+('TJ00001', 'VGA_00001', 'Asus GTX 1080 TI', 1, 15000000, 15000000);
 
 -- --------------------------------------------------------
 
@@ -209,7 +192,30 @@ CREATE TABLE `tb_pembelian` (
 --
 
 INSERT INTO `tb_pembelian` (`notrans`, `tgltrans`, `supplier`, `subtotal`, `diskon`, `total`) VALUES
-('TB00001', '0000-00-00', 'Innovation', 0, 0, 0);
+('TB00001', '0000-00-00', 'ECS', 0, 0, 24550000),
+('TB00002', '0000-00-00', 'Innovation', 0, 0, 15000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penjualan`
+--
+
+CREATE TABLE `tb_penjualan` (
+  `notrans` varchar(10) NOT NULL,
+  `tgltrans` date NOT NULL,
+  `customer` varchar(50) NOT NULL,
+  `subtotal` double(255,0) NOT NULL,
+  `diskon` double(255,0) NOT NULL,
+  `total` double(255,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_penjualan`
+--
+
+INSERT INTO `tb_penjualan` (`notrans`, `tgltrans`, `customer`, `subtotal`, `diskon`, `total`) VALUES
+('TJ00001', '0000-00-00', 'Divine', 0, 0, 15000000);
 
 -- --------------------------------------------------------
 
@@ -295,6 +301,12 @@ ALTER TABLE `tb_inventory`
 -- Indexes for table `tb_pembelian`
 --
 ALTER TABLE `tb_pembelian`
+  ADD PRIMARY KEY (`notrans`);
+
+--
+-- Indexes for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
   ADD PRIMARY KEY (`notrans`);
 
 --
